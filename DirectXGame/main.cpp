18c892a7c4b8e4ed5f26e7b245dc2c26d52f6f47
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-05-06 15:54:19 +0900

    ウィンドウを作った

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,9 +1,20 @@
-#include <Windows.h>
+#include "../Externals/YokosukaEngine/Include/YokosukaEngine.h"
 
 // Windowsアプリでのエントリーポイント（main関数）
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	OutputDebugStringA("Hello World!! \n");
+	// エンジンの生成と初期化
+	YokosukaEngine* yokosukaEngine = new YokosukaEngine();
+	yokosukaEngine->Initialize(1280, 720);
+
+	// メインループ
+	while (yokosukaEngine->ProcessMessage())
+	{
+
+	}
+
+	// エンジンの解放
+	delete yokosukaEngine;
 
 	return 0;
 }
