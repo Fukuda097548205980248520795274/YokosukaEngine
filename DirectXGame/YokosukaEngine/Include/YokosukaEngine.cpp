3f97857730078ae0dd39bdd74bebecd994a15654
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-05-12 16:26:18 +0900

    フォルダの位置を直した

---
+++ DirectXGame/YokosukaEngine/Include/YokosukaEngine.cpp
@@ -0,0 +1,60 @@
+#include "YokosukaEngine.h"
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+YokosukaEngine::~YokosukaEngine()
+{
+	// DirectXCommon
+	delete directXCommon_;
+
+	// ログ出力
+	delete log_;
+
+	// ウィンドウアプリケーション
+	delete windowApplication_;
+
+	// リソースリークチェッカー
+	Microsoft::WRL::ComPtr<IDXGIDebug1> debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug))))
+	{
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+	}
+
+	// COMの終了処理
+	CoUninitialize();
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void YokosukaEngine::Initialize(const int32_t kWindowWidth, const int32_t kWindowHeight)
+{
+	// 乱数を設定する
+	currentTimer_ = static_cast<unsigned int>(time(nullptr));
+	srand(currentTimer_);
+
+
+	// COMの初期化
+	CoInitializeEx(0, COINIT_MULTITHREADED);
+
+	// 出力用のディレクトリを用意する
+	std::filesystem::create_directory("YokosukaEngine/Output");
+
+	// CrashHandlerの登録
+	SetUnhandledExceptionFilter(ExportDump);
+
+	// ウィンドウアプリケーションの生成と初期化
+	windowApplication_ = new WinApp();
+	windowApplication_->Initialize(kWindowWidth, kWindowHeight);
+
+	// ログ出力の生成と初期化
+	log_ = new OutputLog();
+	log_->Initialize();
+
+	// DirectXCommonの生成と初期化
+	directXCommon_ = new DirectXCommon();
+	directXCommon_->Initialize(log_, windowApplication_);
+}
