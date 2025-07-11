1fc6370adddbd9cb61c49908b4d740b6ac693459
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-07-11 11:29:44 +0900

    敵の基底クラスを作成した

---
+++ DirectXGame/Game/GameScene/BaseEnemy/BaseEnemy.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "../../../YokosukaEngine/Include/YokosukaEngine.h"
+
+class BaseEnemy
+{
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="engine"></param>
+	/// <param name="camera3d"></param>
+	/// <param name="position"></param>
+	virtual void Initialize(const YokosukaEngine* engine, const Camera3D* camera3d, const Vector3& position);
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	virtual void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	virtual void Draw() = 0;
+
+
+private:
+
+	// エンジン
+	const YokosukaEngine* engine_ = nullptr;
+
+	// カメラ
+	const Camera3D* camera3d_ = nullptr;
+
+
+	// ワールドトランスフォーム
+	std::unique_ptr<WorldTransform> worldTransform_ = nullptr;
+};
+
