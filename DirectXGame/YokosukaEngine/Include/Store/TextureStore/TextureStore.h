154e5223e62fdc94af73edebaefffe63457abe10
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-07-15 20:32:29 +0900

    一応コミット

--- DirectXGame/YokosukaEngine/Include/Store/TextureStore/TextureStore.h
+++ DirectXGame/YokosukaEngine/Include/Store/TextureStore/TextureStore.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <stdlib.h>
 #include <time.h>
-#include "../../Func/LoadTexture/LoadTexture.h"
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <dxcapi.h>
+#include <cassert>
+#include <vector>
+#include <wrl.h>
+#include "../../Base/Logging/Logging.h"
+#include "../../Func/String/String.h"
+#include "../../../../Externals/DirectXTex/DirectXTex.h"
+#include "../../../../Externals/DirectXTex/d3dx12.h"
 
 // 前方宣言
 class DirectXCommon;
@@ -78,3 +87,49 @@ private:
 	// 格納したテクスチャの数
 	uint32_t textureNum_ = 0;
 };
+
+
+
+/// <summary>
+/// テクスチャデータを読み込む
+/// </summary>
+/// <param name="filePath">ファイルパス</param>
+/// <returns></returns>
+DirectX::ScratchImage LoadTexture(const std::string& filePath);
+
+/// <summary>
+/// テクスチャリソースを作成する
+/// </summary>
+/// <param name="device">デバイス</param>
+/// <param name="metadata">テクスチャのデータ</param>
+/// <returns></returns>
+Microsoft::WRL::ComPtr<ID3D12Resource> CreateTextureResource(Microsoft::WRL::ComPtr<ID3D12Device> device, const DirectX::TexMetadata& metadata);
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="texture"></param>
+/// <param name="mipImages"></param>
+/// <param name="device"></param>
+/// <param name="commandList"></param>
+/// <returns></returns>
+[[nodiscard]]
+Microsoft::WRL::ComPtr<ID3D12Resource> UploadTextureData(Microsoft::WRL::ComPtr<ID3D12Resource> texture, const DirectX::ScratchImage& mipImages,
+	ID3D12Device* device, Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList);
+
+/// <summary>
+/// バッファリソースを作成する
+/// </summary>
+/// <param name="device"></param>
+/// <param name="sizeInBytes"></param>
+/// <returns></returns>
+Microsoft::WRL::ComPtr<ID3D12Resource> CreateBufferResource(ID3D12Device* device, size_t sizeInBytes);
+
+/// <summary>
+/// デプスステンシルテクスチャのリソースを作成する
+/// </summary>
+/// <param name="device"></param>
+/// <param name="width"></param>
+/// <param name="height"></param>
+/// <returns></returns>
+Microsoft::WRL::ComPtr<ID3D12Resource> CreateDepthStencilTextureResource(ID3D12Device* device, int32_t width, int32_t height);
