cdae684e47b6f68af1edf60754d364596eb88653
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-07-16 19:01:37 +0900

    プレイヤーのアニメーションを作成した

--- DirectXGame/Game/GameScene/Player/Player.cpp
+++ DirectXGame/Game/GameScene/Player/Player.cpp
@@ -1,55 +1,127 @@
 #include "Player.h"
 
 /// <summary>
-/// Initialize
+/// 初期化
 /// </summary>
 /// <param name="engine"></param>
 /// <param name="camera3d"></param>
 void Player::Initialize(const YokosukaEngine* engine, const Camera3D* camera3d)
 {
-	// Check for nullptr
+	// nullptrチェック
 	assert(engine);
 	assert(camera3d);
 
-	// Receive arguments
+	// 引数を受け取る
 	engine_ = engine;
 	camera3d_ = camera3d;
 
 
-	// Create and initialize worldTransform
+	// ワールドトランスフォームの生成と初期化
 	worldTransform_ = std::make_unique<WorldTransform>();
 	worldTransform_->Initialize();
 
-	// Create and initialize uvTransform
+	// UVトランスフォーム
 	uvTransform_ = std::make_unique<UvTransform>();
 	uvTransform_->Initialize();
 
-	// Load the modelData
-	modelHandle_ = engine_->LoadModelData("./Resources/Models/Player", "Player.obj");
+
+	// 胴体
+	models_[kBody].worldTransform_ = std::make_unique<WorldTransform>();
+	models_[kBody].worldTransform_->Initialize();
+	models_[kBody].worldTransform_->translation_ = modelsStartPosition[kBody];
+	models_[kBody].worldTransform_->SetParent(worldTransform_.get());
+	models_[kBody].uvTransform_ = std::make_unique<UvTransform>();
+	models_[kBody].uvTransform_->Initialize();
+	models_[kBody].modelHandle_ = engine_->LoadModelData("./Resources/Models/Player/body", "body.obj");
+
+	// 頭
+	models_[kHead].worldTransform_ = std::make_unique<WorldTransform>();
+	models_[kHead].worldTransform_->Initialize();
+	models_[kHead].worldTransform_->translation_ = modelsStartPosition[kHead];
+	models_[kHead].worldTransform_->SetParent(models_[kBody].worldTransform_.get());
+	models_[kHead].uvTransform_ = std::make_unique<UvTransform>();
+	models_[kHead].uvTransform_->Initialize();
+	models_[kHead].modelHandle_ = engine_->LoadModelData("./Resources/Models/Player/head", "head.obj");
+
+	// 左腕
+	models_[kLArm].worldTransform_ = std::make_unique<WorldTransform>();
+	models_[kLArm].worldTransform_->Initialize();
+	models_[kLArm].worldTransform_->translation_ = modelsStartPosition[kLArm];
+	models_[kLArm].worldTransform_->SetParent(models_[kBody].worldTransform_.get());
+	models_[kLArm].uvTransform_ = std::make_unique<UvTransform>();
+	models_[kLArm].uvTransform_->Initialize();
+	models_[kLArm].modelHandle_ = engine_->LoadModelData("./Resources/Models/Player/L_arm", "L_arm.obj");
+
+	// 右腕
+	models_[kRArm].worldTransform_ = std::make_unique<WorldTransform>();
+	models_[kRArm].worldTransform_->Initialize();
+	models_[kRArm].worldTransform_->translation_ = modelsStartPosition[kRArm];
+	models_[kRArm].worldTransform_->SetParent(models_[kBody].worldTransform_.get());
+	models_[kRArm].uvTransform_ = std::make_unique<UvTransform>();
+	models_[kRArm].uvTransform_->Initialize();
+	models_[kRArm].modelHandle_ = engine_->LoadModelData("./Resources/Models/Player/R_arm", "R_arm.obj");
+
+	// 浮遊ギミックの初期化
+	InitializeFloatingGimmick();
+
+	// 手揺れギミックの初期化
+	InitializeHandSwingGimmick();
 }
 
 /// <summary>
-/// Update
+/// 更新処理
 /// </summary>
 void Player::Update()
 {
+	ImGui::Begin("Player");
+	ImGui::SliderFloat3("Head Translation", &models_[kHead].worldTransform_->translation_.x, -10.0f, 10.0f);
+	ImGui::SliderFloat3("LArm Translation", &models_[kLArm].worldTransform_->translation_.x, -10.0f, 10.0f);
+	ImGui::SliderFloat3("RArm Translation", &models_[kRArm].worldTransform_->translation_.x, -10.0f, 10.0f);
+	ImGui::SliderFloat("floating time", &floatingTime_, 0.1f, 3.0f);
+	ImGui::SliderFloat("floating amplitude", &floatingAmplitude_, 0.0f, 3.0f);
+	ImGui::SliderFloat("handSwing time", &handSwingTime_, 0.1f, 3.0f);
+	ImGui::SliderFloat("handSwing amplitude", &handSwingAmplitude_, 0.0f, 3.0f);
+	ImGui::End();
+
+
 	// 入力操作
 	Input();
 
-	// Update worldTransform
-	worldTransform_->UpdateWorldMatrix();
+	// 浮遊ギミックの更新処理
+	UpdateFloatingGimmick();
 
-	// Update uvTransform
+	// 手揺れギミックの更新処理
+	UpdateHandSwingGimmick();
+
+	// トランスフォームを更新する
+	worldTransform_->UpdateWorldMatrix();
 	uvTransform_->UpdateWorldMatrix();
+
+	// モデルを更新する
+	for (uint32_t i = 0; i < kNumModels; i++)
+	{
+		models_[i].worldTransform_->UpdateWorldMatrix();
+		models_[i].uvTransform_->UpdateWorldMatrix();
+	}
 }
 
 /// <summary>
-/// Draw
+/// 描画処理
 /// </summary>
 void Player::Draw()
 {
-	// Draw the model
-	engine_->DrawModel(worldTransform_.get(), uvTransform_.get(), camera3d_, modelHandle_, Vector4(1.0f, 1.0f, 1.0f, 1.0f), false);
+	// モデルを更新する
+	engine_->DrawModel(models_[kBody].worldTransform_.get(), models_[kBody].uvTransform_.get(),
+		camera3d_, models_[kBody].modelHandle_, Vector4(1.0f, 1.0f, 0.4f, 1.0f), false);
+
+	engine_->DrawModel(models_[kHead].worldTransform_.get(), models_[kHead].uvTransform_.get(),
+		camera3d_, models_[kHead].modelHandle_, Vector4(1.0f, 1.0f, 1.0f, 1.0f), false);
+
+	engine_->DrawModel(models_[kLArm].worldTransform_.get(), models_[kLArm].uvTransform_.get(),
+		camera3d_, models_[kLArm].modelHandle_, Vector4(0.4f, 0.4f, 1.0f, 1.0f), false);
+
+	engine_->DrawModel(models_[kRArm].worldTransform_.get(), models_[kRArm].uvTransform_.get(),
+		camera3d_, models_[kRArm].modelHandle_, Vector4(1.0f, 0.4f, 0.4f, 1.0f), false);
 }
 
 
@@ -114,4 +186,66 @@ void Player::Move()
 
 	// 最短角度補間
 	worldTransform_->rotation_.y = LerpShortAngle(worldTransform_->rotation_.y, toRotationY_, 0.2f);
+}
+
+
+/// <summary>
+/// 浮遊ギミック初期化
+/// </summary>
+void Player::InitializeFloatingGimmick()
+{
+	floatingTime_ = 1.0f;
+	floatingParameter_ = 0.0f;
+	floatingAmplitude_ = 0.1f;
+}
+
+/// <summary>
+/// 浮遊ギミック更新処理
+/// </summary>
+void Player::UpdateFloatingGimmick()
+{
+	// 1フレームでのパラメータ加算
+	const float step = 2.0f * std::numbers::pi_v<float> / (floatingTime_ * 60.0f);
+
+
+	// パラメータを1ステップ分加算
+	floatingParameter_ += step;
+
+	// 2πを越えたら0に戻す
+	floatingParameter_ = std::fmod(floatingParameter_, std::numbers::pi_v<float> *2.0f);
+
+
+
+	// 浮遊の振れ幅を座標に反映する
+	models_[kBody].worldTransform_->translation_.y = std::sin(floatingParameter_) * floatingAmplitude_;
+}
+
+
+/// <summary>
+/// 手揺れギミック初期化
+/// </summary>
+void Player::InitializeHandSwingGimmick()
+{
+	handSwingTime_ = 2.0f;
+	handSwingParameter_ = 0.0f;
+	handSwingAmplitude_ = 0.5f;
+}
+
+/// <summary>
+/// 手揺れギミック更新処理
+/// </summary>
+void Player::UpdateHandSwingGimmick()
+{
+	// 1フレームでのパラメータ加算
+	const float step = 2.0f * std::numbers::pi_v<float> / (handSwingTime_ * 60.0f);
+
+	// パラメータを1ステップ分加算
+	handSwingParameter_ += step;
+
+	// 2πを越えたら0に戻す
+	handSwingParameter_ = std::fmod(handSwingParameter_, std::numbers::pi_v<float> *2.0f);
+
+	// 手を揺らす
+	models_[kLArm].worldTransform_->rotation_.x = std::cos(handSwingParameter_) * handSwingAmplitude_;
+	models_[kRArm].worldTransform_->rotation_.x = std::cos(handSwingParameter_) * handSwingAmplitude_;
 }
--- DirectXGame/Game/GameScene/Player/Player.h
+++ DirectXGame/Game/GameScene/Player/Player.h
@@ -6,19 +6,19 @@ class Player
 public:
 
 	/// <summary>
-	/// Initialize
+	/// 初期化
 	/// </summary>
 	/// <param name="engine"></param>
 	/// <param name="camera3d"></param>
 	void Initialize(const YokosukaEngine* engine, const Camera3D* camera3d);
 
 	/// <summary>
-	/// Update
+	/// 更新処理
 	/// </summary>
 	void Update();
 
 	/// <summary>
-	/// Draw
+	/// 描画処理
 	/// </summary>
 	void Draw();
 
@@ -49,26 +49,118 @@ private:
 	void Move();
 
 
-	// engine
+	// エンジン
 	const YokosukaEngine* engine_ = nullptr;
 
-	// camera
+	// カメラ
 	const Camera3D* camera3d_ = nullptr;
 
 	// メインカメラ
 	const MainCamera* mainCamera_ = nullptr;
 
 
-	// worldTransform
+	// ワールドトランスフォーム
 	std::unique_ptr<WorldTransform> worldTransform_ = nullptr;
 
-	// uvTransform
+	// UVトランスフォーム
 	std::unique_ptr<UvTransform> uvTransform_ = nullptr;
 
-	// modelHandle
-	uint32_t modelHandle_ = 0;
 
 	// 目標角度
 	float toRotationY_ = 0.0f;
+
+
+
+	// モデル
+	enum ModelEnum
+	{
+		// 胴体
+		kBody,
+
+		// 頭
+		kHead,
+
+		// 左腕
+		kLArm,
+
+		// 右腕
+		kRArm,
+
+		// モデル数
+		kNumModels
+	};
+
+	// モデル
+	struct ModelStruct
+	{
+		// ワールドトランスフォーム
+		std::unique_ptr<WorldTransform> worldTransform_ = nullptr;
+
+		// UVトランスフォーム
+		std::unique_ptr<UvTransform> uvTransform_ = nullptr;
+
+		// モデルハンドル
+		uint32_t modelHandle_ = 0;
+	};
+
+	// モデル
+	ModelStruct models_[kNumModels];
+
+	// モデルの初期ポジション
+	const Vector3 modelsStartPosition[kNumModels] =
+	{
+		{0.0f , 0.0f , 0.0f},
+		{0.0f , 1.15f , 0.0f},
+		{-0.25f , 1.1f , 0.0f},
+		{0.25f , 1.1f , 0.0f}
+	};
+
+
+	/*----------------
+		浮遊ギミック
+	----------------*/
+
+	/// <summary>
+	/// 浮遊ギミック初期化
+	/// </summary>
+	void InitializeFloatingGimmick();
+
+	/// <summary>
+	/// 浮遊ギミック更新処理
+	/// </summary>
+	void UpdateFloatingGimmick();
+
+	// 浮遊ギミックの時間
+	float floatingTime_ = 1.0f;
+
+	// 浮遊のギミックのパラメータ
+	float floatingParameter_ = 0.0f;
+
+	// 浮遊のギミックの揺れ幅
+	float floatingAmplitude_ = 0.1f;
+
+
+	/*-----------------
+		手揺れギミック
+	-----------------*/
+
+	/// <summary>
+	/// 手揺れギミック初期化
+	/// </summary>
+	void InitializeHandSwingGimmick();
+
+	/// <summary>
+	/// 手揺れギミック更新処理
+	/// </summary>
+	void UpdateHandSwingGimmick();
+
+	// 手揺れギミックの時間
+	float handSwingTime_ = 1.0f;
+
+	// 手揺れギミックのパラメータ
+	float handSwingParameter_ = 0.0f;
+
+	// 手揺れギミックの揺れ幅
+	float handSwingAmplitude_ = 0.12f;
 };
 
